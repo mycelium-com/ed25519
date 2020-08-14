10e6b3a24495a6e7e9e4b7642430719a074896a5
CryptoManiac <balthazar@yandex.ru>
2020-08-15 01:41:13 +0300

    Remove keychain implementation

--- include/ed25519.h
+++ include/ed25519.h
@@ -7,43 +7,6 @@
 extern "C" {
 #endif
 
-static const unsigned int BIP32_EXTKEY_SIZE = 74;
-
-// xpriv
-typedef struct KEYCHAIN_PRIVATE_CTX {
-    unsigned char nDepth;
-    unsigned char vchFingerprint[4];
-    unsigned int nChild;
-    unsigned char chaincode[32];
-    unsigned char key[32];
-    unsigned char pubkey[32];
-} KEYCHAIN_PRIVATE_CTX;
-
-// xpub
-typedef struct KEYCHAIN_PUBLIC_CTX {
-    unsigned char nDepth;
-    unsigned char vchFingerprint[4];
-    unsigned int nChild;
-    unsigned char chaincode[32];
-    unsigned char pubkey[32];
-} KEYCHAIN_PUBLIC_CTX;
-
-void ed25519_keychain_private_rebuild(const KEYCHAIN_PUBLIC_CTX *from, const unsigned char key[32], KEYCHAIN_PRIVATE_CTX *to);
-void ed25519_keychain_private_init(KEYCHAIN_PRIVATE_CTX *ctx, const unsigned char *seed, size_t seed_len);
-void ed25519_keychain_private_derive(const KEYCHAIN_PRIVATE_CTX *ctx, KEYCHAIN_PRIVATE_CTX *child_ctx, unsigned int nChild);
-void ed25519_keychain_private_neuter(const KEYCHAIN_PRIVATE_CTX *ctx, KEYCHAIN_PUBLIC_CTX *public_ctx);
-void ed25519_keychain_private_export(const KEYCHAIN_PRIVATE_CTX *ctx, unsigned char binary[BIP32_EXTKEY_SIZE]);
-void ed25519_keychain_private_import(KEYCHAIN_PRIVATE_CTX *ctx, const unsigned char binary[BIP32_EXTKEY_SIZE]);
-int ed25519_keychain_private_equals(const KEYCHAIN_PRIVATE_CTX *ctx1, const KEYCHAIN_PRIVATE_CTX *ctx2);
-const unsigned char * ed25519_keychain_private_get_key(const KEYCHAIN_PRIVATE_CTX *ctx);
-const unsigned char * ed25519_keychain_private_get_pubkey(const KEYCHAIN_PRIVATE_CTX *ctx);
-
-void ed25519_keychain_public_derive(const KEYCHAIN_PUBLIC_CTX *ctx, KEYCHAIN_PUBLIC_CTX *child_ctx, unsigned int nChild);
-void ed25519_keychain_public_export(const KEYCHAIN_PUBLIC_CTX *ctx, unsigned char binary[BIP32_EXTKEY_SIZE]);
-void ed25519_keychain_public_import(KEYCHAIN_PUBLIC_CTX *ctx, const unsigned char binary[BIP32_EXTKEY_SIZE]);
-int ed25519_keychain_public_equals(const KEYCHAIN_PUBLIC_CTX *ctx1, const KEYCHAIN_PUBLIC_CTX *ctx2);
-const unsigned char * ed25519_keychain_public_get_pubkey(const KEYCHAIN_PUBLIC_CTX *ctx);
-
 inline int ed25519_consttime_equal_4(const unsigned char *x, const unsigned char *y) {
     unsigned char r = 0;
 
