6a16463c1098706c60d08b273eb46aa332e1f984
CryptoManiac <balthazar@yandex.ru>
2020-08-18 06:51:46 +0300

    No need to export ed25519_consttime_equal_32

--- include/ed25519.h
+++ include/ed25519.h
@@ -20,47 +20,6 @@ inline int ed25519_consttime_equal_4(const unsigned char *x, const unsigned char
     return !r;
 }
 
-inline int ed25519_consttime_equal_32(const unsigned char *x, const unsigned char *y) {
-    unsigned char r = 0;
-
-    r = x[0] ^ y[0];
-    #define F(i) r |= x[i] ^ y[i]
-    F(1);
-    F(2);
-    F(3);
-    F(4);
-    F(5);
-    F(6);
-    F(7);
-    F(8);
-    F(9);
-    F(10);
-    F(11);
-    F(12);
-    F(13);
-    F(14);
-    F(15);
-    F(16);
-    F(17);
-    F(18);
-    F(19);
-    F(20);
-    F(21);
-    F(22);
-    F(23);
-    F(24);
-    F(25);
-    F(26);
-    F(27);
-    F(28);
-    F(29);
-    F(30);
-    F(31);
-    #undef F
-
-    return !r;
-}
-
 void ed25519_create_privkey(unsigned char *private_key, const unsigned char *seed);
 void ed25519_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed);
 void ed25519_get_pubkey(unsigned char *public_key, const unsigned char *private_key);
