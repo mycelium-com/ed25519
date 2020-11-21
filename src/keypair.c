3d9408f6a301a941c914e69c2ec819bf1b1bb010
CryptoManiac <balthazar@yandex.ru>
2020-11-21 11:26:09 +0300

    Fix private key validation

--- src/keypair.c
+++ src/keypair.c
@@ -2,6 +2,15 @@
 #include "sha3.h"
 #include "ge.h"
 #include <string.h>
+/**
+  *  Return '1' if the bit value at position y within x is '1' and '0' if it's 0  by ANDing x with
+  *  a bit mask where the bit in y's position is '1' and '0' elsewhere and comparing it to all 0's.
+  *
+  *  Returns '1' in least significant bit position if the value of the bit is '1', '0' if it was '0'.
+  */
+inline static unsigned int read_bit(unsigned int x, unsigned int y) {
+    return ((0u == (x & (1u << y))) ? 0u : 1u);
+}
 
 void ed25519_get_pubkey(unsigned char *public_key, const unsigned char *private_key) {
     ge_p3 A;
@@ -12,11 +21,14 @@ void ed25519_get_pubkey(unsigned char *public_key, const unsigned char *private_
 
 int ed25519_verify_privkey(const unsigned char *private_key) {
     unsigned char key_copy[32];
-    memcpy(key_copy, private_key, 32);
-    key_copy[0] &= 248;
-    key_copy[31] &= 127;
-    key_copy[31] |= 64;
-    return memcmp(key_copy, private_key, 32) == 0;
+    if (0 != read_bit(private_key[0], 5)) {
+        memcpy(key_copy, private_key, 32);
+        key_copy[0] &= 248u;
+        key_copy[31] &= 127u;
+        key_copy[31] |= 64u;
+        return memcmp(key_copy, private_key, 32) == 0;
+    }
+    return 0;
 }
 
 int ed25519_verify_pubkey(const unsigned char *public_key) {
